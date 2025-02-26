a01b0dd5cea5529ebe4b3d3b2ece8d936a722bdf
fabruex <fabruex@gmail.com>
2025-02-26 09:44:10 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <HelloWorld singlewordprop="test1" twowords-prop="test2" three-words-prop="test3" />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  defineProps<{
+    /**
+     * Single word prop
+     */
+    singlewordprop: string
+    /**
+     * Two words prop
+     */
+    twowordsProp: string
+    /**
+     * Three words prop
+     */
+    threeWordsProp: string
+
+  }>()
+</script>
+
+<template>
+  <div>{{ singlewordprop }}</div>
+  <div>{{ twowordsProp }}</div>
+  <div>{{ threeWordsProp }}</div>
+</template>
+
+<style scoped>
+
+</style>
